85ee86ab8e781b4c8dda2c502a6905c2febf5036
Joel <joel@Joels-MBP.localdomain>
2023-01-19 15:56:01 -0600

    Removed potential duplicate keys, removed excess HTML on the key

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -9,9 +9,7 @@ export class AppComponent {
   title = 'covid-portal';
 
   links = [
-    {text: 'Labelling', route: ''},
-    {text: 'Labelled Data', route: ''},
-    {text: 'Status', route: ''},
+
     {text: 'Related Words', route: 'related-words'},
     {text: 'Map', route: 'map'},
     {text: 'Logout', route: ''}
